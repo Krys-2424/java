8c6ed288abd9df9ff548076d52d1c64fb5051d23
Krys-2424 <krystalmarylou@outlook.com>
2026-03-11 12:03:52 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,57 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // variable pour stocker un Etudiant
-        // Déclaration
-        Etudiant etu;
-        // Initialisation / construction
-        etu = new Etudiant();
-
-        Etudiant etu2 = new Etudiant("Bastien", 1990);
-        Etudiant etu3 = new Etudiant(2002);
-
-        System.out.println("Le premier etudiant s'appelle " + etu.getNom());
-        etu.setNom("Claude");
-        System.out.println("Le premier etudiant s'appelle " + etu.getNom());
-
-        // etu3.nom = "f*ck";
-        System.out.println(etu3.getNom());
-        etu3.setNom("f*ck");
-        System.out.println(etu3.getNom());
-
-        System.out.println(etu.toString());
-
-        System.out.println(etu.age());
-        System.out.println(etu2.age());
-        System.out.println(etu3.age());
+        Etudiant etu = new Etudiant();
 
         etu.ajoutNote(3.2f);
         etu.ajoutNote(17.2f);
         etu.ajoutNote(10.2f);
         etu.ajoutNote(11.2f);
-        System.out.println(etu.getNotes());
 
-        System.out.println(etu.avoirNote(1));
-        System.out.println(etu.avoirNote(2));
-        System.out.println(etu.avoirNote(3));
-        System.out.println(etu.avoirNote(0));
+        etu.voirNotes();
+        etu.modifierNote(1, 2.5f);
+        System.out.println();
+        etu.voirNotes();
 
-        //  13.5 → 13.5f (float, pas double)
-        etu.modifierNote(0, 13.5f);
-        //  modifierNote retourne void, on affiche séparément
-        System.out.println(etu.avoirNote(0));
+        System.out.println("Ta moyenne est de " + etu.moyenne());
 
-        //  affichage des moyennes
-        System.out.println("Moyenne de etu   : " + etu.moyenne());
+        System.out.println();
+
+        // Créer 25 étudiants dans un ArrayList
+        ArrayList<Etudiant> etudiants = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 25; i++) {
+            Etudiant e = new Etudiant("Etudiant" + (i + 1),
+             rand.nextInt(2000,2020), // Année de naissance aléatoire entre 2000 et 2020
+             rand.nextInt(1, 100), // Identifiant aléatoire entre 1 et 100
+             rand.nextDouble(1.5, 2.0)); // Taille
+
+            // Leur mettre chacun 5 notes aléatoires (entre 0 et 20)
+            for (int j = 0; j < 5; j++) {
+                e.ajoutNote(rand.nextFloat() * 20);
+            }
+
+            etudiants.add(e);
+        }
+
+        // Afficher la moyenne de chacun
+        float sommeGenerale = 0;
+        for (int i = 0; i < etudiants.size(); i++) {
+            Etudiant e = etudiants.get(i);
+            float moyenne = e.moyenne();
+            sommeGenerale += moyenne;
+            System.out.println(e.getNom() + " -> moyenne : " + String.format("%.2f", moyenne));
+        }
+
+        // Afficher la moyenne générale
+        float moyenneGenerale = sommeGenerale / etudiants.size();
+        System.out.println();
+        System.out.println("Moyenne générale : " + String.format("%.2f", moyenneGenerale));
 
     }
 
